ee24e1bbb0c1a434bb08930cbdef9b132068343e
AlexSal1602 <asalbishvili@hotmail.com>
2023-08-02 15:14:23 +0400

    module3, task2

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -3,12 +3,16 @@ package school.mjc.stage0.module3.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
+        int variable_two = 2000;
+        int int_value = 1000;
+        int variable_three = 4000;
+        int value_two = 3000;
         int variables = 5000;
-        variables = 7000;
-        int value = 90000;
+        int variables_two = 7000;
+        int my_value = 90000;
+        int integer = 100000;
+        int my_integer = 1000000;
+        int my_var = 1234567;
+        int myInteger_two = 1234567;
     }
 }
